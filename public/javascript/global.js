b2b1f2b7f77b2e728889ff3dc74802bf2abcff6f
hbbae <hbbae@wecruitcorp.com>
2022-03-18 16:25:52 +0900

    temp

--- public/javascript/global.js
+++ public/javascript/global.js
@@ -1,4 +1,9 @@
-/**
- * @param {string} id 타겟 id
- */
-const globalClick = (id) => document.getElementById(id).addEventListener("click", () => alert("Hello World!"))
+const loadTemplate = async (name, targetName, replace = false) => {
+    const element = json[name]
+    const {data} = await axios.get(element.url)
+    const template = Handlebars.compile(data)
+    const target = document.getElementById(targetName)
+    const compile = template(element.data);
+    replace ? target.innerHTML = compile : target.innerHTML += compile
+    globalClick(name)
+}
